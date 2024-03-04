3326c9cd7e8e0f16edab03ed4cc8bec92027eeec
sunliangmu <sunliangmu@sensorsdata.cn>
2024-03-04 19:40:30 +0800

    chore: op selectionsort

--- Sort/selectionSort/main.c
+++ Sort/selectionSort/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 void selectionSort(int *arr, int size) {
-  int minIndex;
   for (int i = 0; i < size - 1; i++) {
-    minIndex = i;
+    int minIndex = i;
     for (int j = i + 1; j < size; j++) {
       if (*(arr + j) < *(arr + minIndex)) {
         minIndex = j;
